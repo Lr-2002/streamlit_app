6022c85f3bcfc6ce2dc61c1372768e541dbda31a
Lr-2002 <2629651228@qq.com>
2022-09-03 20:54:21 +0800

    sync

--- main.py
+++ main.py
@@ -2,20 +2,18 @@ import streamlit as st
 import datetime
 from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
 import numpy as np
-import chardet
+
 import pandas as pd
 login = False
 session_state = st.session_state
 
-def get_encoding(file):
-    with open(file, 'rb') as f:
-        return chardet.detect(f.read(1))['encoding']
+
 
 
 # session_state['upload'] = False
 # session_state['hand_input'] = False
 def read_login():
-    with open('./ppp.txt', 'r') as f:
+    with open('./add_on/ppp.txt', 'r') as f:
         login = f.read()
 
         login = bool(int(login))
@@ -23,7 +21,6 @@ def read_login():
         return login
 
 def update_total(full, total):
-    ## todo 修改比例计算
     cate = full['科目名称'].unique()
 
     for i in range(total.shape[0]):
@@ -49,7 +46,7 @@ def update_total(full, total):
     return total
 
 def write_login(login):
-    with open('./ppp.txt','w') as f:
+    with open('./add_on/ppp.txt','w') as f:
         f.write(str(int(login)))
         print('wirted')
 
@@ -247,39 +244,101 @@ def act():
 
 
 def login_page(name, sn):
+    df = session_state['df']
+    name = str(name)
+    sn = str(sn)
     user = st.text_input('请输入您的账号')
     number = st.text_input('请输入您的密码')
-    # print(user, name, number, sn)
-    if str(user) == str(name) and str(number) == str(sn) and st.button('确认登录'):
+    col001, col00, col01 = st.columns(3)
+    with col001:
+        login_button = st.button('确认登录')
+
+    with col00:
+        change_name = st.button('修改用户名')
+        if change_name:
+            session_state['change_name'] = True
+    with col01:
+        change_sn = st.button('修改密码')
+        if change_sn:
+            session_state['change_sn'] = True
+    # print(session_state['change_name'])
+    if 'change_name' in session_state and session_state['change_name']:
+        print(session_state['change_name'])
+        name0 = st.text_input('输入原用户名',value = session_state['name0'], key='name0')
+        name1 = st.text_input('输入新用户名',value = session_state['name1'], key='name1')
+        name2 = st.text_input('再次输入新用户名',value = session_state['name2'], key='name2')
+
+        if (session_state['name0'] == name):
+            change = st.button('确认修改用户名')
+
+            if (session_state['name1'] == session_state['name2']) and session_state['name2'] != '' and change:
+                st.success('用户名修改成功')
+                session_state['df'].at[0, 'name'] = str(name1)
+
+                session_state['change_name'] = False
+
+            elif change:
+                st.warning('两次输入新用户名不同')
+        else:
+            st.error('原用户名错误')
+    else:
+        session_state['name0'] = ''
+        session_state['name1'] = ''
+        session_state['name2'] = ''
+
+    if 'change_sn' in session_state and session_state['change_sn']:
+        print(session_state['change_sn'])
+        sn0 = st.text_input('输入原密码', value=session_state['sn0'], key='sn0')
+        sn1 = st.text_input('输入新密码', value=session_state['sn1'], key='sn1')
+        sn2 = st.text_input('输入新密码', value=session_state['sn2'], key='sn2')
+
+
+
+        if (session_state['sn0'] == sn):
+            change_number = st.button('确认修改密码')
+
+            if (session_state['sn1'] == session_state['sn2']) and session_state['sn2'] != '' and change_number:
+                st.success('密码修改成功')
+                session_state['df'].at[0, 'sn'] = str(sn1)
+
+                session_state['change_sn'] = False
+            elif change_number:
+                st.warning('两次输入新密码不同')
+        else:
+            st.warning('原密码错误')
+
+    else:
+        session_state['sn0'] = ''
+        session_state['sn1'] = ''
+        session_state['sn2'] = ''
+    print('修改结果：',df)
+    df.to_csv('./add_on/admin.csv', index=False)
+
+
+    if str(user) == str(name) and str(number) == str(sn) and login_button:
         login=True
-        st.balloons()
-        print('you have logined')
+        st.success('登录成功')
+        # st.balloons()
+        # print('you have logined')
         session_state['logined'] = True
         # act()
     else:
+        st.error('登录失败')
         login = False
-        print('your number is wrong')
-
-# def name_and_update(text, data, filename):
-#     col1, col2 = st.columns(2)
-#     with col1:
-#         st.subheader(text)
-#     with col2:
-#         csv = convert_df(data)
-#         st.download_button('下载该文件', data=csv, file_name=filename, mime='text/csv')
+        # print('your number is wrong')
 
 
 def show():
     # st.title()
-    full = pd.read_csv('./full.csv', encoding='gbk').fillna(0)
-    total = pd.read_csv('./total.csv', encoding='gbk').fillna(' ')
-    resource = pd.read_csv('resource.csv', encoding='gbk').fillna(0)
+    full = pd.read_csv('./add_on/full.csv', encoding='gbk').fillna(0)
+    total = pd.read_csv('./add_on/total.csv', encoding='gbk').fillna(' ')
+    resource = pd.read_csv('./add_on/resource.csv', encoding='gbk').fillna(0)
     st.title('')
 
     # st.subheader('台面账')
     # two_column()
     total = update_total(full, total)
-    name_and_download('台账',total, 'total.csv')
+    name_and_download('台账',total, './add_on/total.csv')
     # add_row = st.button('增加一行')
     #
     # if add_row:
@@ -343,7 +402,7 @@ function(params) {
 
     ge.configure_pagination(paginationAutoPageSize=False, paginationPageSize=25)
     ge = ge.build()
-    name_and_download('写实账', full, 'full.csv')
+    name_and_download('写实账', full, './add_on/full.csv')
     # st.dataframe(full)
     AgGrid(
         pd.DataFrame(full, columns=full.columns),
@@ -355,7 +414,7 @@ function(params) {
     # add_one_row(full, './full.csv','full_upload', 'full_hand_input', resource)
     # todo 暂时粘贴过来
     df = full
-    filename = './full.csv'
+    filename = './add_on/full.csv'
     col11, col21 = st.columns(2)
     with col11:
         upload = st.button('上传文件')
@@ -379,6 +438,9 @@ function(params) {
             st.text('hand input')
             if 'full_hand_input_dict' in session_state.keys():
                 hand_input_dict = session_state['full_hand_input_dict']
+                if hand_input_dict['单价（元）'][0] != '' and hand_input_dict['数量'][0] != '':
+                    # print()
+                    hand_input_dict['金额'] = [str(float(hand_input_dict['单价（元）'][0]) * float(hand_input_dict['数量'][0]))]
             else:
                 hand_input_dict = {}
                 for i in df.keys():
@@ -422,7 +484,7 @@ function(params) {
 
 
     # st.subheader('资源表')
-    name_and_download('资源表', resource, 'resource.csv')
+    name_and_download('资源表', resource, './add_on/resource.csv')
 
     search = st.text_input('搜索一下')
     if search:
@@ -434,94 +496,93 @@ function(params) {
 
 
 
+    with st.expander('资源表详情'):
+        # st.dataframe(resource)
+        gg = GridOptionsBuilder.from_dataframe(resource)
+        # gg.configure_side_bar()
 
-    # st.dataframe(resource)
-    gg = GridOptionsBuilder.from_dataframe(resource)
-    # gg.configure_side_bar()
-
-    gg.configure_pagination(paginationAutoPageSize=False, paginationPageSize=25)
-    grid = gg.build()
-    grid_edited = AgGrid(
-        pd.DataFrame(resource, columns=resource.columns),
-        width= '100%',
-        gridOptions=grid,
-        # editable=True,
-    )
-    # todo 暂时粘贴过来
-    df = resource
-    filename = './resource.csv'
-    col1, col2 = st.columns(2)
-    with col1:
-        upload = st.button('上传材料表格')
-        if upload:
-            session_state['upload'] = True
-    with col2:
-        hand_input = st.button('手动输入材料')
-        if hand_input:
-            session_state['rs_hand_input'] = True
-    if 'rs_upload' in session_state.keys():
-        if session_state['rs_upload']:
-            file = st.file_uploader('上传你的文件')
-            cirtify = st.button('点击确认')
-            if file is not None and cirtify:
-                st.text('您上传的文件内容如下')
-                data = pd.read_csv(file)
-                st.write(data)
-                print('session_state:', session_state)
-    if 'rs_hand_input' in session_state.keys():
-        if session_state['hand_input']:
-            st.text('rs_hand input')
-            if 'rs_hand_input_dict' in session_state.keys():
-                hand_input_dict = session_state['rs_hand_input_dict']
-            else:
-                hand_input_dict = {}
+        gg.configure_pagination(paginationAutoPageSize=False, paginationPageSize=25)
+        grid = gg.build()
+        grid_edited = AgGrid(
+            pd.DataFrame(resource, columns=resource.columns),
+            width= '100%',
+            gridOptions=grid,
+            # editable=True,
+        )
+        # todo 暂时粘贴过来
+        df = resource
+        filename = './add_on/resource.csv'
+        col1, col2 = st.columns(2)
+        with col1:
+            upload = st.button('上传材料表格')
+            if upload:
+                session_state['upload'] = True
+        with col2:
+            hand_input = st.button('手动输入材料')
+            if hand_input:
+                session_state['rs_hand_input'] = True
+        if 'rs_upload' in session_state.keys():
+            if session_state['rs_upload']:
+                file = st.file_uploader('上传你的文件')
+                cirtify = st.button('点击确认')
+                if file is not None and cirtify:
+                    st.text('您上传的文件内容如下')
+                    data = pd.read_csv(file)
+                    st.write(data)
+                    print('session_state:', session_state)
+        if 'rs_hand_input' in session_state.keys():
+            if session_state['rs_hand_input']:
+                st.text('rs_hand input')
+                if 'rs_hand_input_dict' in session_state.keys():
+                    hand_input_dict = session_state['rs_hand_input_dict']
+                else:
+                    hand_input_dict = {}
+                    for i in df.keys():
+                        hand_input_dict[i] = ['']
+                end_func = ''
+                # print(hand_input_dict)
                 for i in df.keys():
-                    hand_input_dict[i] = ['']
-            end_func = ''
-            # print(hand_input_dict)
-            for i in df.keys():
-                # if i in
-                # hand_input_dict[i] = ['']
-                hand_input_dict[i] = [st.text_input(i, value=hand_input_dict[i][0])]
-                if '物资名称' in hand_input_dict.keys() and hand_input_dict['物资名称'] != [''] and resource is not None:
-                    gotit, return_dict = search_name_resource(hand_input_dict['物资名称'][0], resource)
-                    if gotit:
-                        for j in return_dict.keys():
-                            hand_input_dict[j] = return_dict[j]
-                end_func = i
-            if hand_input_dict[end_func] != '':
-                certi = st.button('确认提交')
+                    # if i in
+                    # hand_input_dict[i] = ['']
+                    hand_input_dict[i] = [st.text_input(i, value=hand_input_dict[i][0])]
+                    if '物资名称' in hand_input_dict.keys() and hand_input_dict['物资名称'] != [''] and resource is not None:
+                        gotit, return_dict = search_name_resource(hand_input_dict['物资名称'][0], resource)
+                        if gotit:
+                            for j in return_dict.keys():
+                                hand_input_dict[j] = return_dict[j]
+                    end_func = i
+                if hand_input_dict[end_func] != '':
+                    certi = st.button('确认提交')
 
-                if certi:
-                    st.success('已经成功提交')
-                    # st.write(hand_input_dict)
-                    df = change_type(df, hand_input_dict)
-                    df.to_csv(filename, encoding='gbk', index=False)
-                    session_state['rs_hand_input'] = False
-            session_state['rs_hand_input_dict'] = hand_input_dict
-            print(hand_input_dict)
+                    if certi:
+                        st.success('已经成功提交')
+                        # st.write(hand_input_dict)
+                        df = change_type(df, hand_input_dict)
+                        df.to_csv(filename, encoding='gbk', index=False)
+                        session_state['rs_hand_input'] = False
+                session_state['rs_hand_input_dict'] = hand_input_dict
+                print(hand_input_dict)
 
-    # todo 暂时结束
+        # todo 暂时结束
 
 
 
     # add_one_row(resource, './resource.csv', 'rs_upload', 'rs_hand_input')
 if __name__ == '__main__':
-    # choice = ['登录', '首页']
-    # # st.sidebar()
+
     if 'logined' not in session_state.keys():
         session_state['logined'] =False
-    user = pd.read_csv('./admin.csv').to_numpy()
-    # print(user)
-    name = user[0][0]
-    sc = user[0][1]
-    # print(name, sc)
-    # login = read_login()
-    print(session_state['logined'])
-    if not session_state['logined']:
-        login_page(name, sc)
 
-    if session_state['logined']:
+    df = pd.read_csv('./add_on/admin.csv')
+    session_state['df']= df
+    print(df)
+    name = df.at[0, 'name']
+    sn = df.at[0, ' sn']
+
+    if 'logined' in session_state.keys()and not session_state['logined']:
+        login_page(name, sn)
+
+    if 'logined' in session_state.keys()and session_state['logined']:
         show()
 
 
